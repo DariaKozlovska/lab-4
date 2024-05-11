c02dc8c9a73df0eecb50b6f84f09ccc3dd34dd9a
DariaKozlovska <odriy.lovska@gmail.com>
2024-05-11 17:32:41 +0200

    change-file

--- skrypt.sh
+++ skrypt.sh
@@ -47,3 +47,35 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+#!/bin/bash
+
+create_logs() {
+    local num_logs=$1
+    for ((i=1; i<=$num_logs; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data: $(date +'%Y-%m-%d %H:%M:%S')" >> "$filename"
+    done
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --logs)
+            if [[ -z $2 ]]; then
+                echo "Brak określonej liczby plików. Użyj --logs <liczba_plików>."
+                exit 1
+            fi
+            create_logs "$2"
+            exit 0
+            ;;
+        *)
+            echo "Nieznana flaga: $key. Użyj --logs <liczba_plików>, aby utworzyć określoną liczbę plików logowych."
+            exit 1
+            ;;
+    esac
+    shift
+done
+
