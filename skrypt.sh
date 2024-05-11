4539b8fd91cabcbbe5871c01a382ac3b91cf135e
DariaKozlovska <odriy.lovska@gmail.com>
2024-05-11 17:28:27 +0200

    change-skrypt

--- skrypt.sh
+++ skrypt.sh
@@ -20,3 +20,30 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+#!/bin/bash
+
+create_logs() {
+    for ((i=1; i<=100; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data: $(date +'%Y-%m-%d %H:%M:%S')" >> "$filename"
+    done
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --logs)
+            create_logs
+            exit 0
+            ;;
+        *)
+            echo "Nieznana flaga: $key. Użyj --logs, aby utworzyć 100 plików logowych."
+            exit 1
+            ;;
+    esac
+    shift
+done
+
