765cee345dbcbd4298103d210619790d8732a564
DariaKozlovska <odriy.lovska@gmail.com>
2024-05-11 18:24:56 +0200

    change file

--- skrypt.sh
+++ skrypt.sh
@@ -8,7 +8,7 @@ while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        --date)
+        --date -d)
             show_date
             exit 0
             ;;
@@ -19,8 +19,6 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
-
-#!/bin/bash
 
 create_logs() {
     for ((i=1; i<=100; i++)); do
@@ -35,7 +33,7 @@ while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        --logs)
+        --logs -l)
             create_logs
             exit 0
             ;;
@@ -46,8 +44,6 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
-
-#!/bin/bash
 
 create_logs() {
     local num_logs=$1
@@ -79,30 +75,45 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-#!/bin/bash
-
 print_help() {
     echo "Użycie: ./skrypt.sh [OPCJE]"
     echo "Dostępne opcje:"
-    echo "  --help      Wyświetla pomoc"
-    echo "  --logs N    Tworzy N plików logowych"
-    echo "  --date      Wyświetla dzisiejszą datę"
+    echo "  --help, -h      Wyświetla pomoc"
+    echo "  --logs N, -l N  Tworzy N plików logowych"
+    echo "  --date, -d      Wyświetla dzisiejszą datę"
 }
 
 while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        --help)
+        --help|-h)
             print_help
             exit 0
             ;;
+        --logs|-l)
+            if [[ -z $2 ]]; then
+                echo "Brak określonej liczby plików. Użyj --logs <liczba_plików> lub -l <liczba_plików>."
+                exit 1
+            fi
+            num_logs="$2"
+            shift
+            ;;
+        --date|-d)
+            echo "Dzisiejsza data: $(date +'%Y-%m-%d')"
+            exit 0
+            ;;
         *)
-            echo "Nieznana flaga: $key. Użyj --help, aby wyświetlić wszystkie możliwe opcje."
+            echo "Nieznana flaga: $key. Użyj --help lub -h, aby wyświetlić wszystkie możliwe opcje."
             exit 1
             ;;
     esac
     shift
 done
+
+if [[ -n $num_logs ]]; then
+    echo "Tworzenie $num_logs plików logowych..."
+    # Tutaj dodaj kod do tworzenia plików logowych
+fi
 
 
